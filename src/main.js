32876ed76e767a7033af1a377f6234888053ad67
maxuebing <maxuebing@inrice.com>
2020-01-28 16:28:40 +0800

    5.3. 添加修改提示

--- src/main.js
+++ src/main.js
@@ -14,9 +14,11 @@ import './components'
 import store from './store'
 // 引入插件
 import VueSweetalert2 from './plugins/vue-sweetalert2'
+import Message from './plugins/message'
 
 // 使用插件
 Vue.use(VueSweetalert2)
+Vue.use(Message)
 
 // 设置 false 以阻止 Vue 在启动时生成生产提示
 Vue.config.productionTip = false
