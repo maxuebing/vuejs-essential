8dab795b63f5eca2a39167216f2a7ff03ab03248
maxuebing <maxuebing@inrice.com>
2020-01-28 08:27:28 +0800

    4.5. 完善登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -9,12 +9,32 @@ const routes = [
     path: '/auth/register',
     name: 'Register',
     component: Register
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/Home')
+  },
+  {
+    path: '*',
+    redirect: '/'
   }
 ]
 
 const router = new Router({
   mode: 'history',
   routes
+})
+
+// 全局前置守卫
+router.beforeEach((to, from, next) => {
+  const auth = router.app.$options.store.state.auth
+
+  if (auth && to.path.indexOf('/auth/') !== -1) {
+    next('/')
+  } else {
+    next()
+  }
 })
 
 export default router
